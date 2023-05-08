d840e93e816684c8c86c725b204492bd1bf59f35
Tom Ootes <tom@ootes.io>
2023-05-08 14:53:33 +0200

    cleaning more

--- src/views/home.vue
+++ src/views/home.vue
@@ -39,48 +39,6 @@ import Card from '../components/Card.vue'
       </div>
     </div>
 
-
-    <!-- <div class="section">
-      <div class="container">
-        <h1 class="flex-center">Opdrachtgevers</h1>
-        <p class="flex-center">Wij hebben eerder samengewerkt met</p>
-        <div class="flex">
-          <a v-bind:href="'#/project/' + project.url" class="client" v-for="project in projects" :key="project">
-            <img :src= "project.img" />
-          </a>
-        </div>
-        <p class="flex-center">
-          <a href="#/projects"><button type="button" class="btn btn-primary">Naar projecten</button></a>
-        </p>
-      </div>
-    </div> -->
-
-    <!-- <div class="section bg-gray-lightest">
-      <div class="container">
-        <h1 class="flex-center">Techniek</h1>
-        <br>
-        <div class="row technique">
-          <div class="col-3 vertical-center">
-            <img src="img/vuejs.png" class="img-fluid" alt="Vue.js">
-          </div>
-          <div class="col-9">
-            <h4 class="heading">Framework</h4>
-            <p>Als framework voor onze projecten gebruiken wij Vue. Vue is een lichtgewicht framework dat niet teveel zijn stempel drukt op een project. Dit zorgt ervoor dat je zelf moet blijven nadenken maar ook dat een project niet teveel aan een framework vastzit. Een project kan zo makkelijker worden omgezet naar een ander framework als dit nodig is. Ook de herbruikbaarheid van code is een stuk hoger als het project niet aan een framework vastzit.</p>
-          </div>
-        </div>
-        <br/>
-        <div class="row technique">
-          <div class="col-3 flex-center">
-            <img src="img/sass.svg" class="img-fluid" alt="Sass">
-          </div>
-          <div class="col-9">
-            <h4 class="heading">Styling</h4>
-            <p>Voor styling gebruiken wij SASS. Deze techniek zorgt ervoor dat we variabelen kunnen gebruiken en efficiënter kunnen werken. De opmaak is hierdoor consistenter en dit werkt door in de gebruikerservaring en de uitstraling.</p>
-          </div>
-        </div>
-      </div>
-    </div> -->
-
     <location></location>
 
   </div>
